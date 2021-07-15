936d61dacc4f4dbfd4566e8e8dca16e6d8e5d0c7
let-robots-reign <zotov.alexei.1@yandex.ru>
2021-07-15 20:57:53 +0400

    converter function (not tested)

--- 04-kafka-to-opcua/convert.py
+++ 04-kafka-to-opcua/convert.py
@@ -29,6 +29,8 @@ TYPE_TO_OPC_TYPE = {
     type(None): VariantType.Null
 }
 
+OPC_VARIABLES = {}
+
 
 def get_kafka_consumer():
     return KafkaConsumer(
@@ -49,20 +51,33 @@ def get_opc_server():
 
     objects = server.get_objects_node()
     namespace = server.register_namespace('Arduino\'s measurements')
+    arduino_sensors = objects.add_object(namespace, 'arduino_sensors')
 
-    return server, objects, namespace
+    return server, arduino_sensors, namespace
 
 ######################################################################
 
 
-def convert_message(msg):
+def convert_message(msg, object, namespace):
     # проходимся по полям сообщения
     # если этого поля нет среди opc переменных, добавляем
     # присваиваем значение opc переменной
-    pass
+    for field, value in msg.items():
+        try:
+            opc_value_type = TYPE_TO_OPC_TYPE[type(value)]
+        except KeyError:
+            print(f'Invalid type: {type(value)}, couldn\t convert to OPC variable', flush=True)
+            continue
 
+        default_value = type(value)()
+        if field not in OPC_VARIABLES:
+            var = object.add_variable(namespace, field, default_value, varianttype=opc_value_type)
+            OPC_VARIABLES[field] = var
+        
+        OPC_VARIABLES[field].set_value(value, varianttype=opc_value_type)
 
 ######################################################################
+
 
 def main():
     consumer = get_kafka_consumer()
@@ -70,7 +85,7 @@ def main():
         print('[ERROR] Couldn\'t connect to Kafka consumer')
         return
 
-    opc_server, objects, namespace = get_opc_server()
+    opc_server, object, namespace = get_opc_server()
     if not opc_server:
         print('[ERROR] Couldn\'t connect to OPC server')
         return
@@ -80,7 +95,7 @@ def main():
     for event in consumer:
         message = event.value
         print(f'Got message from Kafka: {message}', flush=True)
-        convert_message(message)
+        convert_message(message, object, namespace)
         consumer.commit()
         sleep(SLEEP_TIMEOUT)
 
